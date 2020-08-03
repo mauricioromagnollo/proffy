5cd6e2351d55b06875420f25c390c51f45ef0db2
x0n4d0 <romagnolimauricio@gmail.com>
2020-08-03 17:45:33 -0300

    feat: add Routes

--- sources/web/src/App.tsx
+++ sources/web/src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-import Landing from './pages/Landing';
-
 import './assets/styles/global.css';
+
+import Routes from './routes';
 
 const App: React.FC = () => {
   return (
-    <Landing />
+    <Routes />
   );
 }
 
