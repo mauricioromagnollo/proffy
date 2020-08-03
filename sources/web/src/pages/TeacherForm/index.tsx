bb7ba14d66a3f258bbf185d69a4e70d858fb50f4
x0n4d0 <romagnolimauricio@gmail.com>
2020-08-03 17:46:30 -0300

    feat: add empty pages

---
+++ sources/web/src/pages/TeacherForm/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const TeacherForm: React.FC = () => {
+  return (
+    <h1>Teacher Form</h1>
+  );
+}
+
+export default TeacherForm;
---
+++ sources/web/src/pages/TeacherList/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const TeacherList: React.FC = () => {
+  return (
+    <h1>Teacher List</h1>
+  );
+}
+
+export default TeacherList;
