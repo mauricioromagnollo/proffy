23aa830fd821c3471550a35bce6ab939e6e8bdc4
x0n4d0 <romagnolimauricio@gmail.com>
2020-08-04 22:15:26 -0300

    feat: finished initial version of TeacherList page

--- sources/web/src/pages/TeacherList/index.tsx
+++ sources/web/src/pages/TeacherList/index.tsx
@@ -1,8 +1,38 @@
 import React from 'react';
 
+import PageHeader from '../../components/PageHeader';
+import TeacherItem from '../../components/TeacherItem';
+
+import './styles.css';
+
 const TeacherList: React.FC = () => {
   return (
-    <h1>Teacher List</h1>
+    <div id="page-teacher-list" className="container">
+      <PageHeader title="Estes são os proffys disponíveis.">
+        <form id="search-teachers">
+          <div className="input-block">
+            <label htmlFor="subject">Matéria</label>
+            <input type="text" id="subject"/>
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="week_day">Dia da semana</label>
+            <input type="text" id="subject"/>
+          </div>
+          
+          <div className="input-block">
+            <label htmlFor="subject">Hora</label>
+            <input type="text" id="time"/>
+          </div>
+        </form>
+      </PageHeader>
+
+      <main>
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+      </main>
+    </div>
   );
 }
 
