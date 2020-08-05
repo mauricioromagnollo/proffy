0e52b59b23d332a235793c53ea27979adc576bf0
x0n4d0 <romagnolimauricio@gmail.com>
2020-08-04 22:17:51 -0300

    feat: add PageHeader component

--- sources/web/src/pages/TeacherForm/index.tsx
+++ sources/web/src/pages/TeacherForm/index.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 
+import PageHeader from '../../components/PageHeader';
+
 const TeacherForm: React.FC = () => {
   return (
-    <h1>Teacher Form</h1>
+    <div id="page-teacher-form" className="container">
+      <PageHeader title="Que incrível que você quer dar aulas." />
+    </div>
   );
 }
 
