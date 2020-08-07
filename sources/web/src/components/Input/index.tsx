8e92a3b61c74f24c1503de64fbd4ef320812e8ee
x0n4d0 <romagnolimauricio@gmail.com>
2020-08-07 14:59:21 -0300

    feat: add new reusable component

---
+++ sources/web/src/components/Input/index.tsx
@@ -0,0 +1,20 @@
+import React, { InputHTMLAttributes } from 'react';
+
+import './styles.css';
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement>{
+  name: string;
+  label: string;
+  type: string;
+}
+
+const Input: React.FC<InputProps> = ({ name, label, type, ...rest }) => {
+  return (
+    <div className="input-block">
+      <label htmlFor={name}>{label}</label>
+      <input type={type} id={name} {...rest} />
+    </div>
+  );
+}
+
+export default Input;
