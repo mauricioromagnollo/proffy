6a8709161781457c212996cf97acd1a804865031
x0n4d0 <romagnolimauricio@gmail.com>
2020-08-07 15:17:35 -0300

    feat: add total connections from API

--- sources/web/src/pages/Landing/index.tsx
+++ sources/web/src/pages/Landing/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 import logo from '../../assets/images/logo/logo.svg';
@@ -8,20 +8,31 @@ import studyIcon from '../../assets/images/icons/study.svg';
 import giveClassesIcon from '../../assets/images/icons/give-classes.svg';
 import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
 
+import api from '../../services/api';
+
 import './styles.css';
 
 const Landing: React.FC = () => {
+  const [totalConnections, setTotalConnections] = useState(0);
+
+  useEffect(() => {
+    api.get('/connections').then(response => {
+      const { total } = response.data;
+      setTotalConnections(total);
+    })
+  }, []);
+
   return (
     <div id="page-landing">
       <div id="page-landing-content" className="container">
         <div className="logo-container">
-          <img src={logo} alt="Font Proffy Logo Image"/>
+          <img src={logo} alt="Font Proffy Logo"/>
           <h2>Sua plataforma de estudos online.</h2>
         </div>
         
         <img 
           src={landing} 
-          alt="Landing Image" 
+          alt="Landing" 
           className="hero-image"
         />
 
@@ -38,7 +49,7 @@ const Landing: React.FC = () => {
         </div>
 
         <span className="total-connections">
-          Total de 200 conexões já realizadas <img src={purpleHeartIcon} alt="Purple Heart Icon" />
+          Total de {totalConnections} conexões já realizadas <img src={purpleHeartIcon} alt="Purple Heart Icon" />
         </span>
       </div>
     </div>
