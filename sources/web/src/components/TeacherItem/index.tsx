eac0824e0235ab2e5dbe99ad60009ad612bee0ca
x0n4d0 <romagnolimauricio@gmail.com>
2020-08-07 16:39:32 -0300

    feat: added integration to API with axios

--- sources/web/src/components/TeacherItem/index.tsx
+++ sources/web/src/components/TeacherItem/index.tsx
@@ -2,34 +2,56 @@ import React from 'react';
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
+import api from '../../services/api';
+
 import './styles.css';
 
-const TeacherItem: React.FC = () => {
+export interface Teacher {
+  id: number;
+  avatar: string;
+  bio: string;
+  cost: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+};
+
+interface TeacherItemProps {
+  teacher: Teacher
+};
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+
+  function createNewConnection() {
+    api.post('/connections', {
+      user_id: teacher.id
+    });
+  }
+
   return (
     <article className="teacher-item">
       <header>
-        <img src="https://avatars3.githubusercontent.com/u/38081852?s=460&u=9379ef1347e90747ea8ae6a345c30d26fba1146e&v=4" alt="Maurício Romagnoli"/>
+        <img src={teacher.avatar} alt={teacher.name}/>
         <div>
-          <strong>Maurício Romagnoli</strong>
-          <span>Matemática</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
 
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
-        <br /><br />
-        Ipsam officiis sit laborum tenetur magnam quam neque expedita, soluta reprehenderit maiores earum, provident libero rem eligendi officia deleniti consequuntur! Aperiam, debitis!
-      </p>
+      <p>{teacher.bio}</p>
 
       <footer>
         <p>
           Preço/hora
-          <strong>R$ 80,00</strong>
+          <strong>R$ {teacher.cost}</strong>
         </p>
-        <button type="button">
+        <a 
+          target="_blank" 
+          onClick={createNewConnection} 
+          href={`https://wa.we/${teacher.whatsapp}`}>
           <img src={whatsappIcon} alt="WhatsApp Icon"/>
           Entrar em contato
-        </button>
+        </a>
       </footer>
     </article>
   );
