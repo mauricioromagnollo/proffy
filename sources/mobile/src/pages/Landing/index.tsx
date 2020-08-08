6ffda2e4060df75e7c3c066644a164bf7161e25c
x0n4d0 <romagnolimauricio@gmail.com>
2020-08-08 13:00:41 -0300

    feat: added navigation to study pages

--- sources/mobile/src/pages/Landing/index.tsx
+++ sources/mobile/src/pages/Landing/index.tsx
@@ -17,6 +17,10 @@ const Landing: React.FC = () => {
     navigate('GiveClasses');
   }
 
+  function handleNavigateToStudyPages() {
+    navigate('Study');
+  }
+
   return (
     <View style={styles.container}>
       <Image source={landingImg} style={styles.banner} />
@@ -26,7 +30,10 @@ const Landing: React.FC = () => {
       </Text>
 
       <View style={styles.buttonsContainer}>
-        <RectButton style={[styles.button, styles.buttonPrimary]}>
+        <RectButton 
+          onPress={handleNavigateToStudyPages}
+          style={[styles.button, styles.buttonPrimary]}
+        >
           <Image source={studyIcon} />
           <Text style={styles.buttonText}>Estudar</Text>
         </RectButton>
