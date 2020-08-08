821cbdbbf9cc2ae56e00333c775b37abec98f981
x0n4d0 <romagnolimauricio@gmail.com>
2020-08-08 11:45:40 -0300

    feat: added import to AppStack route file

--- sources/mobile/App.tsx
+++ sources/mobile/App.tsx
@@ -1,10 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { StatusBar } from 'expo-status-bar';
 import { AppLoading } from 'expo';
+
 import { Archivo_400Regular, Archivo_700Bold, useFonts } from '@expo-google-fonts/archivo';
 import { Poppins_400Regular, Poppins_600SemiBold } from '@expo-google-fonts/poppins';
 
-import Landing from './src/pages/Landing';
+import AppStack from './src/routes/AppStack';
 
 const App: React.FC = () => {
   let [fontsLoaded] = useFonts({
@@ -19,7 +20,7 @@ const App: React.FC = () => {
   } else {
     return (
       <>
-        <Landing />
+        <AppStack />
         <StatusBar style="light" />
       </>
     );
